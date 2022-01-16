7363a657e10fa7f82c777d7fb0028b42efcded18
Louis Grasset <louisgrasset@protonmail.com>
2022-01-16 23:15:10 +0100

    feat: add PropositionButton

---
+++ src/components/PropositionButton/PropositionButton.tsx
@@ -0,0 +1,12 @@
+interface PropositionButtonProps {
+    /** The onClick callback */
+    onClick: () => void;
+}
+
+export function PropositionButton ({
+    onClick
+}: PropositionButtonProps) {
+    return (
+        <div className="proposition-button" onClick={onClick}>Button</div>
+    )
+}
