ebf9b4205cfcf90ccb3469e6afcf8cede64dc2d7
Louis Grasset <louisgrasset@protonmail.com>
2022-01-16 23:30:57 +0100

    test: cover `PropositionButton`

---
+++ src/components/PropositionButton/PropositionButton.test.tsx
@@ -0,0 +1,27 @@
+import { PropositionButton } from './PropositionButton';
+import { mount } from 'enzyme';
+import { act } from '../../tests/testUtils';
+
+describe('proposition-button', () => {
+    let onClickSpy: any;
+
+    beforeEach(()=> {
+        onClickSpy = jest.fn();
+    })
+
+    it('should render without crash', ()=> {
+        const wrapper = mount(<PropositionButton onClick={onClickSpy}/>);
+
+        expect(wrapper.find(PropositionButton).exists()).toBeTruthy();
+    })
+
+    it('should call the onClick callback when clicked', () => {
+        const wrapper = mount(<PropositionButton onClick={onClickSpy}/>);
+
+        act(()=> {
+            wrapper.find(PropositionButton).simulate('click');
+        })
+        wrapper.update();
+        expect(onClickSpy).toHaveBeenCalledTimes(1);
+    })
+})
