19db2ece02cdb6b970b1ae083217a7cad9e09389
Louis Grasset <louisgrasset@protonmail.com>
2022-01-17 10:28:09 +0100

    chore: add eslint and stylelint

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
---
+++ .stylelintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    extends: [
+        "stylelint-config-standard",
+        "stylelint-config-sass-guidelines"
+    ],
+    defaultSeverity: "warning",
+    rules: {
+        "at-rule-no-unknown": null,
+        "indentation": 4,
+        "max-nesting-depth": 5,
+        "scss/at-rule-no-unknown": null,
+        "selector-class-pattern": "^[a-z][a-z0-9\\-_]*[a-z0-9]$",
+        "selector-max-compound-selectors": 5
+    }
+}
