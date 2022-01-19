cfc11dcda98e229cb0edc96a2d272a18805254ae
Louis Grasset <louisgrasset@protonmail.com>
2022-01-19 03:12:31 +0100

    chore: debug informations

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { useCallback, useEffect, useState } from "react";
 import "./App.scss";
 
 import { PropositionCard } from "./components";
 import { propositions } from "./data/Propositions";
 import { computeScores } from "./services/scores";
+import { StorageService } from "./services/StorageService";
+import { PropositionID, UserAnswer } from "./types";
 
 
 function App() {
 
     console.log(computeScores([1, 1, 2], [[1, - 1, 1]]));
+    const storageService = StorageService.getInstance();
+    const [savedAnswers, setSavedAnswers] = useState<[PropositionID,UserAnswer][]>([]);
+    const refresh = useCallback(()=> setSavedAnswers(storageService.getAnswers() || []), []);
+    useEffect(()=> refresh(), []);
 
     return (
         <div className="app">
@@ -16,6 +23,18 @@ function App() {
                     <PropositionCard key={proposition.id} proposition={proposition}/>
                 ))
             }
+            <pre>
+                PROPOSITIONS :({Array.from(propositions).length})
+                <ul>
+                    {Array.from(propositions).map((p,i) => <li key={i}>{p.content}</li>)}
+                </ul>
+                {
+                    savedAnswers.length === Array.from(propositions).length ? "ALL PROPOSITIONS ANSWERED" : "KEEP ANSWERING PROPOSALS"
+                }
+                <br/>
+                <button onClick={()=> storageService.clear()}>Reset app</button>
+                <button onClick={refresh}>Refresh answers</button>
+            </pre>
         </div>
     );
 }
