8b78ccad5c70797a2e34d90b6ab7c47cb3881399
Louis Grasset <louisgrasset@protonmail.com>
2022-01-24 19:33:25 +0100

    feat: apply theme to `Navigation` icons

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,14 +1,28 @@
 import "./Icon.scss";
 
+import { ReactComponent as IconCandidates }  from "./icons/candidates.svg";
+import { ReactComponent as IconLogo }  from "./icons/logo.svg";
+import { ReactComponent as IconOptions }  from "./icons/options.svg";
+import { ReactComponent as IconPropositions }  from "./icons/propositions.svg";
+import { ReactComponent as IconScores }  from "./icons/scores.svg";
+
+const Icons = {
+    logo: <IconLogo />,
+    candidates: <IconCandidates />,
+    options: <IconOptions />,
+    propositions: <IconPropositions />,
+    scores: <IconScores />
+};
+
 interface IconProps {
     /** The icon name to use from /public/images/*.svg */
-    file: string
+    file: "logo" | "candidates" | "options" | "propositions" | "scores";
 }
 
 export function Icon ({ file }: IconProps) {
     return (
         <div className="icon">
-            <img src={`/images/${file}.svg`}/>
+            {Icons[file]}
         </div>
     );
 }
