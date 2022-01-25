757dcada6759fc06e4ce45fcd03a9367dce5b4a6
yopox <yopoxdev@gmail.com>
2022-01-26 00:32:05 +0100

    feat: add more propositions

--- src/types/Proposition.ts
+++ src/types/Proposition.ts
@@ -6,16 +6,31 @@ export enum PropositionID {
     EPR = "EPR",
     LOGEMENT = "LOGEMENT",
     PASS = "PASS",
+    ECOCIDE = "ECOCIDE",
+    EGALITE_HF = "EGALITE_HF",
+    IMPOT_TRANSITION = "IMPOT_TRANSITION",
+    ISF = "ISF",
+    DESTITUER = "DESTITUER",
+    PRIVATISER = "PRIVATISER",
+    BAC = "BAC",
+    BREVETS = "BREVETS",
+    LIBRE_ECHANGE = "LIBRE_ECHANGE",
+    AAH = "AAH",
+    SOINS = "SOINS",
+    TEMPS = "TEMPS",
+    CLOUD = "CLOUD",
 }
 
 export class Proposition {
     id: PropositionID;
+    category: string;
     content: string;
     description: string;
     revision: number;
 
-    constructor(id: PropositionID, content: string, description: string, revision = 1.0) {
+    constructor(id: PropositionID, category: string, content: string, description: string, revision = 1.0) {
         this.id = id;
+        this.category = category;
         this.content = content;
         this.description = description;
         this.revision = revision;
