8672d6c3dae780730449d2f7b5ab7825741c3c7a
Louis Grasset <louisgrasset@protonmail.com>
2022-01-25 10:36:40 +0100

    feat: optimize score computation and add error message

--- src/routes/Scores/Scores.tsx
+++ src/routes/Scores/Scores.tsx
@@ -1,42 +1,48 @@
 import "./Scores.scss";
 
-import { CandidateScore } from "../../components";
+import { useMemo } from "react";
+import { Link } from "react-router-dom";
+
+import { Button, CandidateScore } from "../../components";
 import { ScoringService } from "../../services";
 import { CandidateID } from "../../types";
-
-// interface ScoresProps {
-//
-// }
 
 /**
  * A route to display scores.
  */
 export function Scores() {
+    const scores = useMemo(()=>
+        (Object.keys(CandidateID) as Array<keyof typeof CandidateID>)
+            .map((id) => {
+                const score = ScoringService.getInstance().computeScore(id as CandidateID);
+                return {
+                    candidate: id,
+                    score: score?.score ?? - 1,
+                };
+            })
+            .filter(value => value.score != - 1)
+            .sort((a, b) => {
+                const seed1 = Math.random() * 100;
+                const seed2 = Math.random() * 100;
+                return (b.score * 100 + seed2) - (a.score * 100 + seed1);
+            }), []
+    );
+
     return (
         <div className="route-scores">
             {
-                (Object.keys(CandidateID) as Array<keyof typeof CandidateID>)
-                    // Compute scores
-                    .map((id) => {
-                        const score = ScoringService.getInstance().computeScore(id as CandidateID);
-                        return {
-                            candidate: id,
-                            score: score?.score ?? - 1,
-                        };
-                    })
-                    // Remove bad entries
-                    .filter(value => value.score != - 1)
-                    // Sort candidates by score
-                    .sort((a, b) => {
-                        const seed1 = Math.random() * 100;
-                        const seed2 = Math.random() * 100;
-                        return (b.score * 100 + seed2) - (a.score * 100 + seed1);
-                    })
-                    // Candidate components
-                    .map((obj) => (
-                        <CandidateScore candidateID={obj.candidate} key={obj.candidate}/>
-                    )
-                    )
+                scores.length ?
+                    scores.map((score) => (
+                        <CandidateScore candidateID={score.candidate} key={score.candidate}/>
+                    ))
+                    : <>
+                        <h1>Oups!</h1>
+                        <p>Vous n'avez encore assez voté sur les propositions pour que nous calculions vos affinités. </p>
+                        <br>
+                        </br><Link to="/app/propositions">
+                            <Button>Voir les propositions</Button>
+                        </Link>
+                    </>
             }
         </div>
     );
