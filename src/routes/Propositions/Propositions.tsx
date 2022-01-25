92e715dae16407f26353979a0652b03dfd0bc81b
Louis Grasset <louisgrasset@protonmail.com>
2022-01-25 09:38:10 +0100

    fix: clean code

--- src/routes/Propositions/Propositions.tsx
+++ src/routes/Propositions/Propositions.tsx
@@ -2,7 +2,7 @@ import "./Propositions.scss";
 
 import { useState } from "react";
 
-import { PropositionCard } from "../../components";
+import { Button, PropositionCard } from "../../components";
 import { StorageService } from "../../services";
 import { PropositionID } from "../../types";
 
@@ -19,8 +19,6 @@ export function Propositions() {
     const [proposition, setProposition] = useState<PropositionID>(order[0]);
 
     const getNextProposition = () => {
-        console.log(propositionNb);
-        console.log("Next");
         if (propositionNb == order.length - 1) {
             // TODO: Navigate to score page
             setPropositionNb(0);
@@ -42,12 +40,12 @@ export function Propositions() {
     return (
         <div className="route-propositions">
             <h1 className="header">Proposition – {propositionNb + 1} / {order.length}</h1>
-            <button onClick={reset}>Reset</button>
             {
                 proposition && (
                     <PropositionCard key={proposition} propositionID={proposition} onClick={getNextProposition}/>
                 )
             }
+            <Button type="secondary" onClick={reset}>Reset answers</Button>
         </div>
     );
 }
