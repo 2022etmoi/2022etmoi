de0bd7df83851506997752b68468dd218e1247a3
yopox <yopoxdev@gmail.com>
2022-01-26 01:29:32 +0100

    fix: add missing field to `PropositionMock`

--- src/tests/mocks/PropositionMock.ts
+++ src/tests/mocks/PropositionMock.ts
@@ -1,6 +1,8 @@
 import { Proposition, PropositionID } from "../../types";
+
 export const PropositionMock: Proposition = new Proposition(
     PropositionID.SMIC,
+    "Category",
     "Proposition content",
     "Proposition description",
 );
