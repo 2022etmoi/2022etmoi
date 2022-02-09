2144f8ea396837d00afb20f91fe3a6ec7ef39cf3
Louis Grasset <louisgrasset@protonmail.com>
2022-02-09 01:20:11 +0100

    feat: refresh `CandidateScore`; adapt mixins

--- src/components/CandidateScore/CandidateScore.tsx
+++ src/components/CandidateScore/CandidateScore.tsx
@@ -1,10 +1,12 @@
 import "./CandidateScore.scss";
 
+import { useMemo } from "react";
 import CountUp from "react-countup";
 
 import { candidates } from "../../data/Candidates";
 import { ScoringService } from "../../services";
 import { CandidateID } from "../../types";
+import { Button } from "../Button";
 
 interface CandidateScoreProps {
     candidateID: string,
@@ -14,22 +16,40 @@ interface CandidateScoreProps {
  * A component showing the given candidate.
  * @param id. The current Candidate.
  */
-export function CandidateScore({ candidateID }: CandidateScoreProps) {
+export function CandidateScore({ candidateID }: CandidateScoreProps)  {
     const candidate = candidates.get(candidateID as CandidateID);
-    if (candidate === undefined) return null;
-
     const score = ScoringService.getInstance().computeScore(candidateID as CandidateID);
-    if (score === null) return null;
 
+    const imageFileName = useMemo(()=> candidate?.name.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/ /g, "_").toLowerCase(), [candidate?.name]);
     return (
         <div className="candidate-score">
-            <div className="candidate-score__score">{<CountUp end={Math.round(score.score)} duration={1.5} useEasing/>}</div>
-            <div className="candidate-score__details">
-                <div className="candidate-score__details__name">{candidate.name}</div>
-                <div className="candidate-score__details__movement">{candidate.movement}</div>
-            </div>
-            <div className="candidate-score__chip">{score.hearts} 🥰</div>
-            <div className="candidate-score__chip">{score.skulls} 😡️</div>
+            {
+                candidate && score && (
+                    <>
+                        <div className="candidate-score__image">
+                            <img src={`/images/candidates/${imageFileName}.webp`}/>
+                        </div>
+                        <div className="candidate-score__details">
+                            <div className="candidate-score__details-name">{candidate.name}</div>
+                            <div className="candidate-score__details-movement">{candidate.movement}</div>
+                            <div className="candidate-score__details-score">{<CountUp delay={0.25} end={Math.round(score.score)} duration={1.5} useEasing/>}</div>
+                        </div>
+                        <div className="candidate-score__bars">
+                            <div className="candidate-score__bars-item candidate-score__bars-item--must_not"></div>
+                            <div className="candidate-score__bars-item candidate-score__bars-item--not"></div>
+                            <div className="candidate-score__bars-item candidate-score__bars-item--skip"></div>
+                            <div className="candidate-score__bars-item candidate-score__bars-item--yes"></div>
+                            <div className="candidate-score__bars-item candidate-score__bars-item--must"></div>
+                        </div>
+                        <div className="candidate-score__footer">
+                            <div className="candidate-score__chips">
+                                <div className="candidate-score__chips-item">{score.skulls} <span>❌</span></div>
+                                <div className="candidate-score__chips-item">{score.hearts} <span>😍</span></div>
+                            </div>
+                            <Button type="transparent">En savoir plus</Button>
+                        </div>
+                    </>
+                )}
         </div>
     );
 }
