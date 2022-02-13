e6799958ab1549be4ea577aae2b3a69a93fc1f9a
Louis Grasset <louisgrasset@protonmail.com>
2022-02-13 18:12:17 +0100

    feat: enhance `Button` typing

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,21 +1,27 @@
 import "./Button.scss";
 
-import { ReactNode } from "react";
+import { ComponentProps, ReactNode } from "react";
 
-interface ButtonProps {
+interface ButtonProps extends Omit<ComponentProps<"button">, "type"> {
     /** The button type */
-    type?: "primary" | "secondary" | "transparent",
+    type?: "primary" | "secondary" | "transparent" | "danger",
     /** The callback to call when clicked */
     onClick?: () => void
     /** The button content */
     children: ReactNode
-    /** The css class to inject */
-    className?: string
+    /** The size of the Button */
+    size?: "default" | "small"
 }
 
-export function Button ({ type = "primary", onClick, children, className }: ButtonProps) {
+export function Button ({ type = "primary", onClick, children, className, size, ...buttonProps }: ButtonProps) {
     return (
-        <button onClick={onClick} className={`button${(type === "secondary" ? " button--secondary" :"")}${(type === "transparent" ? " button--transparent" :"")}${className? " "+className :""}`}>
+        <button onClick={onClick} {...buttonProps } className={
+            `button${(type === "secondary" ? " button--secondary" :"")}
+            ${(type === "transparent" ? " button--transparent" :"")}
+            ${(type === "danger" ? " button--danger" :"")}
+            ${(size === "small" ? " button--small" :"")}
+            ${className? " "+className :""}`
+        }>
             {children}
         </button>
     );
