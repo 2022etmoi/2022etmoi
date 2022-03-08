cada38fbd373515a0a16d7fea0cf1dec80092a2c
yopox <yopoxdev@gmail.com>
2022-03-08 15:51:31 +0100

    feat: improve Table

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -4,13 +4,16 @@ import { useMemo } from "react";
 
 import { ReactComponent as IconCandidates } from "./icons/candidates.svg";
 import { ReactComponent as IconLogo } from "./icons/logo.svg";
+import { ReactComponent as IconNeutral } from "./icons/neutral.svg";
+import { ReactComponent as IconNo } from "./icons/no.svg";
 import { ReactComponent as IconOptions } from "./icons/options.svg";
 import { ReactComponent as IconPropositions } from "./icons/propositions.svg";
 import { ReactComponent as IconTable } from "./icons/table.svg";
+import { ReactComponent as IconYes } from "./icons/yes.svg";
 
 export interface IconProps {
     /** The icon name to use from /src/components/Icon/icons/*.svg */
-    file: "logo" | "candidates" | "options" | "propositions" | "scores" | "table";
+    file: "logo" | "candidates" | "options" | "propositions" | "scores" | "table" | "yes" | "no" | "neutral";
 }
 
 export function Icon({ file }: IconProps) {
@@ -18,11 +21,22 @@ export function Icon({ file }: IconProps) {
         switch (file) {
         case "logo":
             return <IconLogo/>;
-        case "candidates": return <IconCandidates />;
-        case "options": return <IconOptions />;
-        case "propositions": return <IconPropositions />;
-        case "table": return <IconTable />;
-        default: return null;
+        case "candidates":
+            return <IconCandidates/>;
+        case "options":
+            return <IconOptions/>;
+        case "propositions":
+            return <IconPropositions/>;
+        case "table":
+            return <IconTable/>;
+        case "yes":
+            return <IconYes/>;
+        case "no":
+            return <IconNo/>;
+        case "neutral":
+            return <IconNeutral/>;
+        default:
+            return null;
         }
     }, [file]);
 
--- src/routes/Table/Table.tsx
+++ src/routes/Table/Table.tsx
@@ -2,28 +2,40 @@ import "./Table.scss";
 
 import { useMemo } from "react";
 
+import { Icon } from "../../components";
 import { candidates } from "../../data/Candidates";
 import { propositions } from "../../data/Propositions";
-import { CandidateAnswer, CandidateID, PropositionID } from "../../types";
+import { StorageService } from "../../services";
+import { CandidateAnswer, CandidateID, PropositionID, UserAnswer } from "../../types";
 
 /**
  * A route to display all answers.
  */
 export function Table() {
-    const candidatesNames = useMemo(()=>
+    const candidatesNames = useMemo(() =>
         Object.keys(CandidateID).map(id => (
             <th className="route-table__wrapper__candidate"
                 key={id}>{candidates.get(id as CandidateID)?.name ?? "‼️"}</th>))
     , []);
 
-    const candidatesAnswers= useMemo(()=>
-        propositions.map(p => (
-            <tr  key={p.id}>
+    const answers = StorageService.getInstance().getAnswers();
+
+    const candidatesAnswers = useMemo(() =>
+        propositions.map(p => [p.id, (
+            <tr key={p.id}>
                 <td>{p.content}</td>
                 {Object.keys(CandidateID).map(id => (<td key={id}>{answer(p.id, id as CandidateID)}</td>))}
             </tr>
-        ))
+        )])
     , []);
+
+    function filterAnswers(answer: UserAnswer) {
+        const ids = answers?.filter(v => v[1] === answer);
+        const filteredAnswers = ids?.map(v => candidatesAnswers.filter(c => c[0] === v[0]));
+        if (filteredAnswers === undefined) return null;
+        if (filteredAnswers.length === 0) return null;
+        return filteredAnswers.filter(a => a.length > 0).map(a => a[0][1]);
+    }
 
     return (
         <div className="route-table">
@@ -39,7 +51,16 @@ export function Table() {
                         </tr>
                     </thead>
                     <tbody>
-                        {candidatesAnswers}
+                        <div className="route-table__wrapper__section">Vraiment pour</div>
+                        {filterAnswers(UserAnswer.MUST)}
+                        <div className="route-table__wrapper__section">Vraiment contre</div>
+                        {filterAnswers(UserAnswer.MUST_NOT)}
+                        <div className="route-table__wrapper__section">Pour</div>
+                        {filterAnswers(UserAnswer.YES)}
+                        <div className="route-table__wrapper__section">Contre</div>
+                        {filterAnswers(UserAnswer.NO)}
+                        <div className="route-table__wrapper__section">Ne se prononce pas</div>
+                        {filterAnswers(UserAnswer.NEUTRAL)}
                     </tbody>
                 </table>
             </div>
@@ -47,26 +68,19 @@ export function Table() {
     );
 }
 
-enum TableAnswer {
-    YES = "🟢",
-    NO = "🔴",
-    NEUTRAL = "🤐️",
-    MISSING = "⚠️",
-}
-
 function answer(proposition: PropositionID, id: CandidateID) {
     const candidate = candidates.get(id);
-    if (candidate === undefined) return TableAnswer.MISSING;
+    if (candidate === undefined) return (<Icon file="neutral"/>);
 
     const answer = candidate.opinion.get(proposition);
-    if (answer === undefined) return TableAnswer.MISSING;
+    if (answer === undefined) return (<Icon file="neutral"/>);
 
     switch (answer.value) {
     case CandidateAnswer.NO:
-        return TableAnswer.NO;
+        return (<Icon file="no"/>);
     case CandidateAnswer.YES:
-        return TableAnswer.YES;
+        return (<Icon file="yes"/>);
     case CandidateAnswer.NEUTRAL:
-        return TableAnswer.NEUTRAL;
+        return (<Icon file="neutral"/>);
     }
 }
