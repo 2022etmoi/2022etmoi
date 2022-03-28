dc9b7511bdb0e8b8f2fbff2f87eef84e6ae7cbd1
yopox <yopoxdev@gmail.com>
2022-03-28 17:33:24 +0200

    fix: add `MemoryRouter` to Table test

--- src/routes/Table/Table.test.tsx
+++ src/routes/Table/Table.test.tsx
@@ -1,10 +1,15 @@
 import { mount } from "enzyme";
+import { MemoryRouter } from "react-router-dom";
 
 import { Table } from "./Table";
 
 describe("table", () => {
     it("should render without crash", () => {
-        const wrapper = mount(<Table/>);
+        const wrapper = mount(
+            <MemoryRouter initialEntries={["/"]}>
+                <Table/>
+            </MemoryRouter>
+        );
         expect(wrapper.find(Table).exists()).toBeTruthy();
     });
 });
